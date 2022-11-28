d0cdc816a5f4114a63c337d07469c5e7ae0f7081
Dmitriy <ampilog1@gmail.com>
2022-11-28 12:38:16 +0300

    Создаем переменные для заливки в модели

---
+++ sqlalchemy_lesson18/Data_for_model.py
@@ -0,0 +1,81 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sqlite3
+
+# Подключение к базе данных
+conn = sqlite3.connect('TestDB.db')
+
+# Создаем курсор
+cursor = conn.cursor()
+
+cursor.execute('SELECT * from Region')
+
+result = cursor.fetchall()
+print(result)
+print(type(result))
+
+# engine = create_engine('sqlite:///orm.sqlite', echo=True)
+#
+# Base = declarative_base()
+#
+#
+# class Region(Base):
+#     __tablename__ = 'region'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     number = Column(Integer, nullable=True)
+#
+#
+#     def __init__(self, name, number):
+#         self.name = name
+#         self.number = number
+#
+#     def __str__(self):
+#         return f'{self.id}) {self.name}: {self.number}'
+#
+#
+#
+#
+# Base.metadata.create_all(engine)
+#
+# Session = sessionmaker(bind=engine)
+#
+# session = Session()
+#
+# region = Region('Москва', 1)
+#
+# session.add(region)
+#
+# region = Region('Петербург', 2)
+#
+# session.add(region)
+#
+# session.commit()
+#
+# region.name = 'Тула'
+#
+# session.commit()
+#
+# session.delete(region)
+#
+# session.commit()
+#
+# for i in range(10):
+#     region = Region(f'region{i}', i)
+#     session.add(region)
+#
+# session.commit()
+#
+# region_query = session.query(Region)
+#
+# print(type(region_query))
+#
+# for region in region_query:
+#     print(region.name)
+#
+# regions = session.query(Region).all()
+#
+# print(type(regions))
+#
+# regions = session.query(Region).filter(Region.name == 'Москва' and Region.id > 14).all()
